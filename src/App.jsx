169d923b3a58abae9364fd0745d65df3d32aa92d
Darrenvandervelde <Darrenvandervelde@gmail.com>
2026-04-16 14:35:30 +0100

    Fix import statement for LoginPage component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LoginPage } from './components/Login';
+import LoginPage from './components/Login';
 import './App.css';
 
 function App() {
