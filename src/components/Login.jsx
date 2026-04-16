a2fd6c2ee0705e3aabc67de6bfced6db2e3737c9
Darrenvandervelde <Darrenvandervelde@gmail.com>
2026-04-16 14:56:49 +0100

    Add validation checks for username and password in LoginPage component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,13 +2,31 @@ import { useState } from "react";
 import "./Login.css";
 
 function LoginPage() {
+  /* UseState For Password and Username */
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
+    if (!username.trim() || !password.trim()) {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    if(username.length < 3) {
+        alert("Username needs to be at least 3 letters");
+        return;
+    }
+
+    if(password.length < 3) {
+        alert("Password needs to be at least 3 letters");
+        return;
+    }
+    
     console.log("Username:", username);
     console.log("Password:", password);
+
   };
 
   return (
